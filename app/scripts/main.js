e325cf9b99e907fa3f9eb20796b30c71705181a7
supercrabtree <supercrabtree@googlemail.com>
2014-03-09 12:46:23 +1100

    added title and svg background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,6 @@
   var canvas = document.getElementById('canvas')
     , content = canvas.getContext('2d');
 
-
   function draw() {
     
   }
@@ -12,7 +11,6 @@
   function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    
   }
 
   window.addEventListener('resize', function (e) {
@@ -21,5 +19,7 @@
 
   resizeCanvas();
 
+  var title = '#t1|#t2|#t3|#t4|#t5|#t6|#t7|#t8|#t9|#t10|#t11|'.split('|');
+  TweenMax.staggerFrom(title, 1, {alpha: 0, y: '50', scaleX: 0, scaleY: 0, ease: Power4.easeOut, delay: 1}, 0.03);
 
 })(Boid, Vector);
